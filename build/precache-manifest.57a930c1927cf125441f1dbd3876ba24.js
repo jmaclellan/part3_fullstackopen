e5cf4ec8d5fecaaed9907bc1bab361a07373de31
John MacLellan <jmaclellan24@gmail.com>
2020-12-08 17:34:53 -0500

    Fixed build:ui pathways and created build

---
+++ build/precache-manifest.57a930c1927cf125441f1dbd3876ba24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a15070c35583cfeceedbcfa7de7416a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f2d54b62183afd7082",
+    "url": "/static/css/main.91c980e3.chunk.css"
+  },
+  {
+    "revision": "20fe075eb47146d3a9b5",
+    "url": "/static/js/2.23fddf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23fddf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f2d54b62183afd7082",
+    "url": "/static/js/main.7d49a973.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
