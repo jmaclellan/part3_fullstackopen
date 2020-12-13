401b62e163dc5e8ffd4e1fc9a97fd13549a717a3
John MacLellan <jmaclellan24@gmail.com>
2020-12-12 20:08:55 -0500

    Added error handling routes

--- index.js
+++ index.js
@@ -86,6 +86,25 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+// error handling
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
