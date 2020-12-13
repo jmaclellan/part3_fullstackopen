f8f90b494df88e595b71de5542d630edf3292af8
John MacLellan <jmaclellan24@gmail.com>
2020-12-12 20:00:27 -0500

    Remove array of test data

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan('tiny'))
 
-const persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-]
-
 // generate id function
 const generateId = () => Math.floor(Math.random() * 1000000)
 
