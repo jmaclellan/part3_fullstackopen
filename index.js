743d34d7910ad06ab46cda146dd68fa0528c61ae
John MacLellan <jmaclellan24@gmail.com>
2020-12-16 22:16:11 -0500

    Use dotenv when not in production

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 
 const app = express()
-require('dotenv').config()
 
 const morgan = require('morgan')
 const cors = require('cors')
